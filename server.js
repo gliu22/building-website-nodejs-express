5febbd2c0e18455b0d86e5e75a09fac71c177077
gliu22 <justdoitken@gmail.com>
2020-08-09 21:33:32 -0500

    set uo server.js and install eslint

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+
+const port = 3000;
+
+app.use(express.static(path.join(__dirname,'./provided/static')));
+app.get('/', (request, response) =>{
+    response.sendFile(path.join(__dirname, './provided/static/index.html'));
+});
+
+app.get('/speakers', (request, response) =>{
+    response.sendFile(path.join(__dirname, './provided/static/speakers.html'));
+});
+
+app.listen(port, ()=>{
+    console.log(`Express Server Listing port ${port}.`);
+});
